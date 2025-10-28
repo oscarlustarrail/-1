44726b694d3441b6bf69c712c7f5e31495c28a1e
oscarlustarrail <oscarlu.starrail@gmail.com>
2025-10-28 09:33:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 # HolleWorld add
-  說你好
+1.說你好
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
 	cout<<"Hollow World";
 }
-  加法
+2.加法
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
