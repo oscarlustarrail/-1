33fb37b50baaefb501cb2ada3637c1ac544ba10f
oscarlustarrail <oscarlu.starrail@gmail.com>
2025-10-28 09:31:44 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+***HolleWorld add
+  說你好
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	cout<<"Hollow World";
+}
+  加法
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	double a, b;
+	cin >> a >> b;
+	cout << (a+b);
+}
+
